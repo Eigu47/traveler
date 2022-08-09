4f497f7c71ed1d0449ba68f0e757aa24563a6d7c
Eiguchi Pablo <pablo.eiguchi@gmail.com>
2022-08-09 16:25:27 +0900

    folder structure

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,9 @@
+export default function Navbar() {
+  return (
+    <div>
+      <div className="h-10 w-screen bg-indigo-600">
+        <h2 className="p-3 text-2xl text-slate-50">Traveler.</h2>
+      </div>
+    </div>
+  );
+}
