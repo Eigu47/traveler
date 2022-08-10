6e42c9f6bde18c6bc8deebda04700f213a8cfa4a
Eiguchi Pablo <pablo.eiguchi@gmail.com>
2022-08-10 12:52:39 +0900

    folder structure and refining gps

---
+++ pages/_app.tsx
@@ -0,0 +1,21 @@
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
+import Navbar from "../components/Navbar";
+import Head from "next/head";
+
+export default function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <>
+      <Head>
+        <title>TRAVELER</title>
+        <meta
+          name="description"
+          content="Search and find your favorites places to visit"
+        />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <Navbar />
+      <Component {...pageProps} />
+    </>
+  );
+}
---
+++ pages/map.tsx
@@ -0,0 +1,9 @@
+import Map from "../components/map/Map";
+
+export default function map() {
+  return (
+    <main className="relative top-14 h-[calc(100vh-56px)] max-h-full max-w-full">
+      <Map />
+    </main>
+  );
+}
