f8880c1acad28c63fc1f060c3446d327f4ca09ec
Eiguchi Pablo <pablo.eiguchi@gmail.com>
2022-08-10 23:32:16 +0900

    before implementing real search api

--- components/map/SearchBar.tsx
+++ components/map/SearchBar.tsx
@@ -2,8 +2,8 @@ import usePlacesAutocomplete from "use-places-autocomplete";
 import { Combobox, Transition } from "@headlessui/react";
 
 import { FiSearch } from "react-icons/fi";
-import { MdPlace } from "react-icons/md";
 import { Fragment, useState } from "react";
+import { read } from "fs";
 
 const people = [
   { id: 1, name: "Wade Cooper" },
@@ -35,23 +35,32 @@ export default function SearchBar() {
 
   return (
     <div className="absolute top-4 right-4 z-10 w-72 text-sm">
-      <Combobox value={selectedPlace} onChange={setSelectedPlace}>
+      <Combobox
+        value={selectedPlace}
+        onChange={setSelectedPlace}
+        nullable
+        disabled={!ready}
+      >
         <form className="relative flex w-full overflow-hidden rounded-lg bg-white px-3 py-2 shadow-lg ring-1 ring-black ring-opacity-5">
           <Combobox.Input
             className="w-full outline-none"
+            displayValue={(person: any) => person?.name}
             onChange={(e: any) => setQuery(e.target.value)}
+            placeholder="Search city..."
           />
           <button type="submit" className="text-xl">
             <FiSearch />
           </button>
         </form>
         <Transition
+          as={Fragment}
           enter="transition ease-in duration-100"
           enterFrom="transform -translate-y-5 opacity-0"
           enterTo="transform translate-y-0 opacity-100"
           leave="transition ease-in duration-100"
           leaveFrom="transform translate-y-0 opacity-100"
           leaveTo="transform -translate-y-5 opacity-0"
+          afterLeave={() => setQuery("")}
         >
           <Combobox.Options className="absolute mt-1 max-h-60 w-full transform divide-y divide-black/5 overflow-auto rounded-lg bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5">
             {filteredPeople.length === 0 && query !== "" && (
