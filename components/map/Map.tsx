6a442e5f404735633609c2e258badaaf7eb47f43
Eiguchi Pablo <pablo.eiguchi@gmail.com>
2022-08-10 12:59:34 +0900

    folder structure

--- components/map/Map.tsx
+++ components/map/Map.tsx
@@ -2,24 +2,29 @@ import { GoogleMap, useLoadScript } from "@react-google-maps/api";
 import { useCallback, useRef, useState } from "react";
 import { MdGpsFixed } from "react-icons/md";
 
-function getCurrentPosition() {
-  navigator?.geolocation?.getCurrentPosition((pos) => {
-    const position = {
-      lat: pos.coords.latitude,
-      lng: pos.coords.longitude,
-    };
-    return position;
-  });
-}
-
 export default function Map() {
   const [center, setCenter] = useState({ lat: 35.6762, lng: 139.6503 });
-  const mapRef = useRef();
+  const mapRef = useRef(null);
 
-  const onLoad = useCallback((map) => (mapRef.current = map), []);
+  const getCurrentPosition = useCallback(() => {
+    navigator?.geolocation?.getCurrentPosition((pos) => {
+      setCenter({
+        lat: pos.coords.latitude,
+        lng: pos.coords.longitude,
+      });
+    });
+  }, []);
+
+  const onLoad = useCallback(
+    (map) => {
+      mapRef.current = map;
+      getCurrentPosition();
+    },
+    [getCurrentPosition]
+  );
 
   const { isLoaded } = useLoadScript({
-    googleMapsApiKey: process.env.NEXT_PUBLIC_MAP_API_KEY,
+    googleMapsApiKey: process.env.NEXT_PUBLIC_MAP_API_KEY as string,
   });
 
   if (!isLoaded) {
@@ -41,7 +46,7 @@ export default function Map() {
       ></GoogleMap>
       {navigator.geolocation && (
         <button
-          onClick={() => console.log(mapRef.current)}
+          onClick={getCurrentPosition}
           className="absolute bottom-7 right-3 rounded-lg border border-black/30 bg-white p-1 text-xl text-gray-500 shadow-md duration-75 ease-in-out hover:text-black"
         >
           <MdGpsFixed />
