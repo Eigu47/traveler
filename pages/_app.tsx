c272d0888469d7d027dbe22c55614c01a7a4547d
Eiguchi Pablo <pablo.eiguchi@gmail.com>
2022-08-20 18:22:42 +0900

    mobile vh

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,12 +5,26 @@ import Head from "next/head";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { useLoadScript } from "@react-google-maps/api";
+import { useEffect } from "react";
 
 export default function MyApp({ Component, pageProps }: AppProps) {
   const { isLoaded } = useLoadScript({
     googleMapsApiKey: process.env.NEXT_PUBLIC_MAP_API_KEY as string,
     libraries,
   });
+
+  useEffect(() => {
+    function addStyle() {
+      const vh = window.innerHeight * 0.01;
+      document.documentElement.style.setProperty("--vh", `${vh}px`);
+    }
+
+    window.addEventListener("resize", addStyle);
+
+    return () => {
+      window.removeEventListener("resize", addStyle);
+    };
+  }, []);
 
   return (
     <>
