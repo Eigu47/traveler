10c2776e400f422bea90736a03dcf26731b0f1d0
Eiguchi Pablo <pablo.eiguchi@gmail.com>
2022-08-28 22:17:40 +0900

    trying w/o mongo adapter

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -4,7 +4,7 @@ import { MongoDBAdapter } from "@next-auth/mongodb-adapter";
 import clientPromise from "../../../utils/mongodb";
 
 export default NextAuth({
-  adapter: MongoDBAdapter(clientPromise),
+  // adapter: MongoDBAdapter(clientPromise),
   providers: [
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID as string,
